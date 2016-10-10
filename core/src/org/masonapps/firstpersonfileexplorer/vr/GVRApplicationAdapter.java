f43a4069cceeb1d8fa29245c1d396295fca36831
bobm3 <mason94275@gmail.com>
2016-10-10 16:33:29 -0400

    switched to ApplicationListener from ApplicationAdapter

--- core/src/org/masonapps/firstpersonfileexplorer/vr/GVRApplicationAdapter.java
+++ core/src/org/masonapps/firstpersonfileexplorer/vr/GVRApplicationAdapter.java
@@ -1,11 +1,40 @@
 package org.masonapps.firstpersonfileexplorer.vr;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 
 /**
  * Created by Bob on 10/9/2016.
  */
 
-public abstract class GVRApplicationAdapter extends ApplicationAdapter {
+public class GVRApplicationAdapter implements ApplicationListener {
 
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
